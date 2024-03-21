517ccca4f118938fd22dd1179de5ffee16ef457a
iq-00 <subhasbharathi@gmail.com>
2024-03-21 09:43:45 +0530

    backend folder initizialize

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/" element={<LandingPage />} /> */}
 
           <Route
             path="/signin"
